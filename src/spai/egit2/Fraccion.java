22ebcbe2123a72015737582ae1d540b6f850df17
Mark Cheptea <m.celmare@gmail.com>
2013-10-16 22:51:51 +0200

    Implemented the `Fraccion.isPropia()`, `Fraccion.isImpropia` and `Fraccion.equivalente()`

--- src/spai/egit2/Fraccion.java
+++ src/spai/egit2/Fraccion.java
@@ -68,15 +68,24 @@ public class Fraccion {
     }
     
 	public boolean isPropia() {
-		return true;
+		if(this.numerador < this.denominador){
+			return true;
+		} else return false;
+		
 	}
 
 	public boolean isImpropia() {
-		return true;
+		if(this.isPropia()){
+			return false;
+		} return true;
 	}
 
 	public boolean equivalente(Fraccion fraccion) {
-		return true;
+		
+		if(fraccion.getNumerador() * this.denominador == fraccion.getDenominador() * this.numerador){
+			return true;
+		} else return false;
+		
 	}
 
 }
